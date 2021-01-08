f917ea40c8fa8693b2b6f9388104dd2804951818
Binh Mai <1753030@student.hcmus.edu.vn>
2021-01-09 05:00:51 +0700

    Edit api upload

--- api/wish-list/controllers/wish-list.js
+++ api/wish-list/controllers/wish-list.js
@@ -5,4 +5,17 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          data.author = ctx.state.user.id;
+          entity = await strapi.services.wish-list.create(data, { files });
+        } else {
+          ctx.request.body.author = ctx.state.user.id;
+          entity = await strapi.services.wish-list.create(ctx.request.body);
+        }
+        return sanitizeEntity(entity, { model: strapi.models.wish-list });
+      },
+};
