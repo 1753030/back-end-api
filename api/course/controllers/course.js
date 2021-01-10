83900de31ca241d759531844153acdea61a78204
Binh Mai <1753030@student.hcmus.edu.vn>
2021-01-10 21:15:52 +0700

    find courses by student id

--- api/course/controllers/course.js
+++ api/course/controllers/course.js
@@ -1,6 +1,6 @@
 'use strict'
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
-const qs = require('qs');
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils')
+const qs = require('qs')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
@@ -61,7 +61,7 @@ module.exports = {
     return sanitizeEntity(entity, { model: strapi.models.course })
   },
   async find(ctx) {
-    console.log(ctx.query);
+    console.log(ctx.query)
     let entities
     if (ctx.query._q) {
       entities = await strapi.services.course.search(ctx.query)
@@ -96,40 +96,59 @@ module.exports = {
         }
       })
     }
-    return course;
+    return course
   },
   async findByStudent(ctx) {
-    const { id } = ctx.params;
-    
+    const { id, findByStudent } = ctx.params
+    console.log(ctx.params)
+
     const entities = await strapi.services['student-course'].find({
-        'userId.id': ctx.state.user.id,
-    });
+      'userId.id': ctx.state.user.id,
+    })
+
+    console.log(entities.length);
+
+    if (findByStudent === 'findOneByStudent') {
+      let flag = false
+      entities.map((entity) => {
+        if (entity.courseId.id === id) {
+          flag = true;
+        }
+      })
+
+      if (!flag) {
+        return ctx.unauthorized(`You can't read this entry`)
+      }
+      console.log('H')
+      const entity = await strapi.services.course.findOne({ id })
+      const course = sanitizeEntity(entity, { model: strapi.models.course })
+      return course
+    }
 
     let listIdCourse = {
-        id_in: [],
-      };
-    entities.map(entity => {
-        listIdCourse.id_in.push(
-            entity.courseId.id
-        );
+      id_in: [],
+    }
+
+    entities.map((entity) => {
+      listIdCourse.id_in.push(entity.courseId.id)
     })
-    
-    console.log("course.findByStudent: " + listIdCourse);
-    const entity = await strapi.services.course.find(listIdCourse);
+
+    console.log('course.findByStudent: ' + listIdCourse)
+    const entity = await strapi.services.course.find(listIdCourse)
     // return ctx.query;
-    return sanitizeEntity(entity, { model: strapi.models.course });
+    return sanitizeEntity(entity, { model: strapi.models.course })
   },
   async create(ctx) {
-    let entity;
+    let entity
     if (ctx.is('multipart')) {
-      const { data, files } = parseMultipartData(ctx);
-      data.instructorId = ctx.state.user.id;
-      entity = await strapi.services.course.create(data, { files });
+      const { data, files } = parseMultipartData(ctx)
+      data.instructorId = ctx.state.user.id
+      entity = await strapi.services.course.create(data, { files })
     } else {
-      ctx.request.body.instructorId = ctx.state.user.id;
-      entity = await strapi.services.course.create(ctx.request.body);
+      ctx.request.body.instructorId = ctx.state.user.id
+      entity = await strapi.services.course.create(ctx.request.body)
     }
-    return sanitizeEntity(entity, { model: strapi.models.course });
+    return sanitizeEntity(entity, { model: strapi.models.course })
   },
   //   async findByStudent(ctx) {
   //     console.log(ctx.params)
